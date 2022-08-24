36f5212d3f6bbda0448c6e4fbbc9a888ccf1fabc
Adriel Barria <barria.adriel66@gmail.com>
2022-08-24 16:22:11 -0300

    Update Header

--- ArgentinaPrograma_Proyecto_adrielba/src/app/app.module.ts
+++ ArgentinaPrograma_Proyecto_adrielba/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgComponent } from './components/logo-arg/logo-arg.component';
+import { SocialNetsComponent } from './components/social-nets/social-nets.component';
+import { MybannerComponent } from './components/mybanner/mybanner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgComponent,
+    SocialNetsComponent,
+    MybannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ ArgentinaPrograma_Proyecto_adrielba/src/app/components/logo-arg/logo-arg.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogoArgComponent } from './logo-arg.component';
+
+describe('LogoArgComponent', () => {
+  let component: LogoArgComponent;
+  let fixture: ComponentFixture<LogoArgComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogoArgComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LogoArgComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
