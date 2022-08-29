9cf0dbd55db67e9bb2a630914096a85719640d54
Adriel Barria <barria.adriel66@gmail.com>
2022-08-29 08:06:26 -0300

    Componente Footer

--- ArgentinaPrograma_Proyecto_adrielba/src/app/app.module.ts
+++ ArgentinaPrograma_Proyecto_adrielba/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EducationComponent } from './components/education/education.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectsComponent } from './components/proyects/proyects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ProyectsComponent } from './components/proyects/proyects.component';
     ExperienceComponent,
     EducationComponent,
     SkillsComponent,
-    ProyectsComponent
+    ProyectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
